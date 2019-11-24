439be66fb21c317b703a1825565dfb52c2d03cc7
Anna Morris <annapatton444@gmail.com>
2019-11-23 18:40:07 -0600

    Login: bug but moveing on

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from "./Components/Login"
 
 function App() {
   return (
     <div className="App">
-      I'm an app
+      <h1>30before30</h1>
+      <Login></Login>
     </div>
   );
 }
---
+++ react1/src/Components/Login.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import api from "../utils/axiosWithAuth";
+
+const Login = ({ touched, errors, status, handleSubmit }) => {
+  const [login, setLogin] = useState([]);
+
+  useEffect(() => {
+    if (status) {
+      setLogin([...login, status]);
+      localStorage.setItem("loggedIn", "true");
+    }
+  }, [status]);
+
+  return (
+    <div>
+       <h2>Login here!</h2>
+      <Form onSubmit={handleSubmit}>
+        {" "}
+        <label>
+          {" "}
+          Name:
+          <Field type="text" name="username" placeholder="Username" />
+        </label>
+        <label>
+          {" "}
+          Password:
+          <Field type="password" name="password" placeholder="Password" />
+          {touched.password && errors.password && (
+            <p className="error">{errors.password}</p>
+          )}
+        </label>
+        <button type="submit">Login</button>{" "}
+      </Form>{" "}
+    </div>
+  );
+};
+
+const FormikLogin = withFormik({
+  mapPropsToValues({ username, password }) {
+    return {
+      username: username || "",
+      password: password || ""
+    };
+  },
+
+  validationSchema: Yup.object().shape({
+    username: Yup.string()
+      .required(),
+    password: Yup.string()
+      .min(6)
+      .max(12)
+      .required()
+  }),
+
+  handleSubmit(values, { props, resetForm, setSubmitting, setStatus }) {
+    api
+      .post("/auth/login", {}, { auth: values })
+      .then(response => {
+        setStatus(response.data);
+        props.loggedStatus();
+        resetForm();
+        setSubmitting(false);
+        props.history.push("/home");
+      })
+      .catch(err => console.log(err.response));
+  }
+})(Login);
+
+export default FormikLogin;
