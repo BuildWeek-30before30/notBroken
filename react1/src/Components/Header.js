fd6e13a5785daa8e858526c217cac8808cb1e76b
Anna Morris <annapatton444@gmail.com>
2019-11-28 14:21:22 -0600

    trying to impliment conditional rendering

--- react1/src/Components/Header.js
+++ react1/src/Components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import api from "../utils/axiosWithAuth"
-import {Button} from "reactstrap"
+import api from "../utils/axiosWithAuth";
+import { Button } from "reactstrap";
 
 function Header() {
+  const isLoggedIn = localStorage.loggedIn;
   return (
     <div className="Header">
       <header>
@@ -12,16 +13,29 @@ function Header() {
             <li>
               <Link to="/">Login</Link>
             </li>
+            {/* {localStorage.loggedIn === true && (
+              <li>
+                <Link to="/list">Lists</Link>
+              </li>
+            )} */}
             <li>
               <Link to="/list">Lists</Link>
             </li>
           </ul>
         </nav>
-        {/* This gets a positive response from the api. So it is correct I just don't know how to make it work with Redux */}
-        <Button onClick={()=>{
-          api.get('/auth/logout', { username: 'goodusername', password: 'goodpassword' })
-          .then(response => console.log(response))
-        }}>
+        {/* This gets a positive response from the api. So it is correct I just don't know how to make it work without Redux */}
+        <Button
+          onClick={() => {
+            api
+              .get("/auth/logout", {
+                username: "goodusername",
+                password: "goodpassword"
+              })
+              .then(response => {console.log(response)
+                localStorage.setItem("loggedIn", "false");}
+              );
+          }}
+        >
           Logout
         </Button>
       </header>
